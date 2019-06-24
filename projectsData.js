cfc35cb0e33f2302d6e3a1d392ecb13073109efa
Darin Hennigh <dchennigh@gmail.com>
2019-06-24 19:45:24 -0400

    First commit

---
+++ projectsData.js
@@ -0,0 +1,28 @@
+const projectsData = [
+    {
+        id: 1,
+        name: "Gougères",
+        description: "Recipe for making cheese gougères"
+    },
+    {
+        id: 2,
+        name: "Châlets and Caviar",
+        description: "Real estate website utilizing Wordpress, CSS, and HTML"
+
+    },
+    {
+        id: 3,
+        name: "Brooklyn Outdoor Film Festival", 
+        description: "Outdoor film festival in Brooklyn utilizing HTML, Bootstrap, and CSS"
+
+    },
+    {
+        id: 4,
+        name: "Professional Portfolio", 
+        description: "React website showcasing my professional portfolio"
+
+    },
+
+]
+
+export default projectsData
