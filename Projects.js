7839f400b3d9d86fec952bc4fa80b08979b0398a
Darin Hennigh <dchennigh@gmail.com>
2019-06-25 17:19:35 -0400

    adding Projects component

---
+++ Projects.js
@@ -0,0 +1,21 @@
+import React from "react"
+import projectsData from "./projectsData"
+
+class Projects extends React.Component {
+    render() {
+        return (
+            <div>
+            	let i = 0;
+                {projectsData.map(project => (
+                	<Projects
+                		key={'project${i++}'}
+                		name={project.name},
+                		description={project.description}
+                	/>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Projects
--- ShellComponent.js
+++ ShellComponent.js
@@ -1,21 +1,13 @@
 import React from "react"
-import projectsData from "./projectsData"
 
-class Projects extends React.Component {
+class ComponentName extends React.Component {
     render() {
         return (
             <div>
-            	let i = 0;
-                {projectsData.map(project => (
-                	<Projects
-                		key={'project${i++}'}
-                		name={project.name},
-                		description={project.description}
-                	/>
-                ))}
+            	
             </div>
         )
     }
 }
 
-export default Projects
+export default ComponentName
