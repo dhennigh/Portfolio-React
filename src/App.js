a0fbf45b0ef6b6d74da71db1d1f845233822d794
Darin Hennigh <dchennigh@gmail.com>
2019-07-16 22:25:00 -0500

    added some styling and working on hamburger icon toggle

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import Social from "./Social"
 import Navbar from "./Navbar"
 import Introduction from "./Introduction"
 import ContactFormComponent from "./ContactFormComponent"
-
+import AboutMe from "./AboutMe"
+import Portfolio from "./Portfolio"
+import Pdf from "./Resume.pdf"
 import Resume from "./Resume"
 
+
 import './style.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
 
 
 class App extends React.Component {
     render() {
         return (
             <div>
-            	<Introduction />
+            	<Router>
+            		<Navbar />
+					<Switch>
+						<Route exact path='/' component={Introduction} />
+				        <Route path='/about/' component={AboutMe} />
+				        <Route path='/portfolio/' component={Portfolio} />
+				        <Route path='/contact/' component={ContactFormComponent} />
+				        <Route path='/resume/' component={Pdf} />
+				    </Switch>
+				</Router>
             </div>
         )
     }
--- src/Hamburger.js
+++ src/Hamburger.js
@@ -1,25 +1,5 @@
 import React from "react"
 
-class ClosedButton extends React.Component {
-	render() {
-		return(
-			<div>
-            	<i className="fa fa-bars fa-2x hamburgerComponent" onClick={this.HamburgerToggle}></i>
-            </div>
-		)
-	}
-}
-
-class OpenedButton extends React.Component {
-	render() {
-		return(
-			<div>
-            	<i className="far fa-window-close hamburgerComponent" onClick={this.HamburgerToggle}></i>
-            </div>
-		)
-	}
-}
-
 class Hamburger extends React.Component {
 	constructor(props) {
 		super(props)
@@ -27,32 +7,19 @@ class Hamburger extends React.Component {
 			isOpen: false,
 		}
 		this.HamburgerToggle = this.HamburgerToggle.bind(this)
-		this.menuButton
-	}
-
-	const menuButton
-
-	HamburgerToggle() {
-		this.setState((prevState) => {
-			this.state.isOpen=!prevState.isOpen
-		})
-		
-		if(this.state.isOpen === false) {
-			menuButton = <ClosedButton />
-		} else {
-			menuButton = <OpenedButton />
-		}
 	}
 
     render() {
+    	const iconClassName = this.state.isOpen ? "far fa-window-close fa-2x" : "fa fa-bars fa-2x"
+
+    	console.log("I'm working")
+
         return (
             <div>
-            	{this.menuButton}
-            	<i className="fa fa-bars fa-2x hamburgerComponent" onClick={this.HamburgerToggle}></i>
-            	<i className="far fa-window-close"></i>
+            	<i className={`${iconClassName} hamburger`} onClick={this.props.handleClick}></i>
             </div>
         )
     }
 }
 
-export default { Hamburger, ClosedButton, OpenedButton }
+export default Hamburger
